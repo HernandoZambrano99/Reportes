75bcf4d53323a5c994aadee602d14af06d7ca275
hernando <hernandozambrano556@gmail.com>
2025-09-16 11:50:24 -0500

    Se agrega model solicitud aprobada

---
+++ domain/model/src/main/java/co/com/bancolombia/model/solicitudaprobada/SolicitudAprobada.java
@@ -0,0 +1,20 @@
+package co.com.bancolombia.model.solicitudaprobada;
+import lombok.*;
+//import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class SolicitudAprobada {
+    private Integer solicitudId;
+    private String estado;
+    private String correoCliente;
+    private String nombreCliente;
+    private String tipoPrestamo;
+    private Integer plazo;
+    private BigDecimal monto;
+}
