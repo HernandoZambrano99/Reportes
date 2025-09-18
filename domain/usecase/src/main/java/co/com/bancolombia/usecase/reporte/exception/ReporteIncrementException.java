f871fd04b8a2f54e15cc37da2910729b26ed5e43
hernando <hernandozambrano556@gmail.com>
2025-09-17 22:25:57 -0500

    Se modifica map para responder

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/reporte/exception/ReporteIncrementException.java
+++
@@ -1,7 +0,0 @@
-package co.com.bancolombia.usecase.reporte.exception;
-
-public class ReporteIncrementException extends RuntimeException {
-    public ReporteIncrementException(Integer solicitudId, String detail, Throwable cause) {
-        super("Error incrementando reporte para solicitud " + solicitudId + ": " + detail, cause);
-    }
-}
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/constants/AppConstants.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/constants/AppConstants.java
@@ -8,6 +8,6 @@ public class AppConstants {
     public static final String LOG_GET_REPORTE_TRACE = "GET " + API_REPORTE_PATH + " trace={}";
     public static final String LOG_GET_REPORTE_ERROR = "Error en GET " + API_REPORTE_PATH;
     public static final String FIELD_ID = "id";
-    public static final String FIELD_TOTAL_SOLICITUDES_APROBADAS = "total solicitudes aprobadas";
-    public static final String FIELD_TOTAL_MONTO_CREDITOS = "total monto creditos";
+    public static final String FIELD_TOTAL_SOLICITUDES_APROBADAS = "totalSolicitudesAprobadas";
+    public static final String FIELD_TOTAL_MONTO_CREDITOS = "totalMontoCreditos";
 }
